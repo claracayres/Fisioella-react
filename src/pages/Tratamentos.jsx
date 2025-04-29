5b2fd23c3ce3feea8daa1383b621630908b2bd07
Maria Clara Cayres <0123m.m4567@gmail.com>
2025-04-29 19:51:44 -0400

    Test

--- src/pages/Tratamentos.jsx
+++ src/pages/Tratamentos.jsx
@@ -81,7 +81,7 @@ const Tratamentos = () => {
                         Eletroterapia<br /> 
                         Laserterapia <br /> 
                         Exercícios específicos para musculatura  do assoalho pélvico <br />
-                        Terapia <br/>comportamental <br />
+                        Terapia comportamental <br />
                         Entre outros 
                     </p>
                 </div>
